87ac0c396773b72f0981ed48939ffc15e6aad938
YAMT <94518822+yetanothermightytool@users.noreply.github.com>
2022-04-25 10:22:25 +0200

    Update vbo-health-checker.ps1

--- vbo/vbo-health-checker/vbo-health-checker.ps1
+++ vbo/vbo-health-checker/vbo-health-checker.ps1
@@ -11,13 +11,13 @@
     Author     : Stephan Herzig, Veeam Software  (stephan.herzig@veeam.com)
     Requires   : PowerShell 
 .VERSION
-    1.3
+    1.4
 #>
 param(
     [Parameter(mandatory=$true)]
     [String] $Organization,
     [Parameter(Mandatory = $false)]
-    [String] $Logfile = "C:\Temp\vbo_healthcheck_$env:computername.log",
+    [String] $Logfile = "C:\Users\stephan.herzig\Documents\Script\vbo\vbo_healthcheck_$env:computername.log",
     [Switch] $Webcheck)
 Clear-Host
 
@@ -25,13 +25,14 @@ Clear-Host
 $orga = $exch = $exar = $od = $sites = $teams = $nok = 0
 $vbo_version = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("C:\Program Files\Veeam\Backup365\Veeam.Archiver.Proxy.exe").FileVersion
 if($Webcheck){
-$WebReq                    = Invoke-WebRequest -Uri https://www.veeam.com/kb4106 
+$WebReq                    = Invoke-WebRequest -Uri https://www.veeam.com/kb4106
 $InnerText                 = $WebReq.AllElements | Where-Object {$_.tagName -eq "TD" -and $_.innerText -ne $null} | Select -ExpandProperty innerText
 $vbo_latestversion         = $InnerText[14]
 $vbo_latestdisplayversion  = $InnerText[16]
 $vbo_latestversionkb       = $InnerText[18]
                    }
 $vbo_org         = Get-VBOOrganization -Name $Organization
+$vbo_bkpapp      = Get-VBOBackupApplication -Organization $vbo_org
 $vbo_logpath     = "C:\ProgramData\Veeam\Backup365\Logs\" + $vbo_org
 $vbo_license     = Get-VBOLicensedUser
 $vbo_exp         = Get-VBOLicense
@@ -51,6 +52,7 @@ $vbo_restore     = Get-VBORestoreSession | Where-Object {($_.StartTime.Hour -lt 
 if (!$logfile) {
 $logfile = "C:\Users\stephan.herzig\Documents\Script\vbo\vbo_healthcheck_$env:computername.log"
 }
+
 #WriteLog Function
 function WriteLog
 {
@@ -64,46 +66,46 @@ WriteLog "*** Start VBO Health Check ***"
 # Start the script
 Write-Host "*******************************************************" -ForegroundColor Cyan
 Write-Host "*                 VBO HEALTH-CHECKER                  *"
-Write-Host "**************************************************v1.3*" -ForegroundColor Cyan
+Write-Host "**************************************************v1.4*" -ForegroundColor Cyan
 
 # Get Backup Job Configuration
-Write-Host "Backup Jobs" -ForegroundColor Cyan
+Write-Host "Health Backup Jobs" -ForegroundColor Cyan
 $jobs = (Get-VBOJob -Organization $vbo_org| Select-Object $_.Name)
-Write-Host "Number of Backup Jobs      " -NoNewline
+Write-Host "Number of Backup Jobs        " -NoNewline
 Write-Host $jobs.Count -ForegroundColor Green
 Write-Host ""
 ForEach ($j in $jobs) {
   $job_items = (Get-VBOJob -Name $j.Name)
-  Write-Host "Job Name                   " -NoNewline
+  Write-Host "Job Name                     " -NoNewline
   Write-Host $j.Name -ForegroundColor Cyan
 
 # Get what got selected within the backup jobs
   If ($job_items.SelectedItems.Organization.Count -ge 1) {
-  "Number of Organizations    "+$job_items.SelectedItems.Organization.Count
+  "Number of Organizations      "+$job_items.SelectedItems.Organization.Count
   $orga=$orga+$vbo_license.Count}
   If($job_items.SelectedItems.User.Count -ge 1 -And $job_items.SelectedItems.Mailbox -eq "True") {
-  "Number of Exchange Users   "+$job_items.SelectedItems.User.Count
+  "Number of Exchange Users     "+$job_items.SelectedItems.User.Count
   $exch=$exch+$job_items.SelectedItems.User.Count}
   ElseIf($job_items.SelectedItems.User.Count -ge 1 -And $job_items.SelectedItems.ArchiveMailbox -eq "True") {
-  "Number of Exchange Archive "+$job_items.SelectedItems.Mailbox.Count
+  "Number of Exchange Archive   "+$job_items.SelectedItems.Mailbox.Count
   $exar+=$job_items.SelectedItems.Mailbox.Count}        
   ElseIf($job_items.SelectedItems.User.Count -ge 1 -And $job_items.SelectedItems.OneDrive -eq "True") {
-  "Number of OneDrive         "+$job_items.SelectedItems.User.Count
+  "Number of OneDrive           "+$job_items.SelectedItems.User.Count
   $od=$od+$job_items.SelectedItems.User.Count}
   ElseIf($job_items.SelectedItems.Site.Count -ge 1) {
-  "Number of Sharepoint Sites "+$job_items.SelectedItems.Site.Count
+  "Number of Sharepoint Sites   "+$job_items.SelectedItems.Site.Count
   $sites=$sites+$job_items.SelectedItems.Site.Count}
   ElseIf($job_items.SelectedItems.Team.Count -ge 1) {
-  "Number of MS Teams Sites   "+$job_items.SelectedItems.Team.Count
+  "Number of MS Teams Sites     "+$job_items.SelectedItems.Team.Count
   $teams=$teams+$job_items.SelectedItems.Team.Count}
-  Write-Host "Last Backup Status         " -NoNewline 
+  Write-Host "Last Backup Status           " -NoNewline 
   Write-Host $job_items.LastStatus -ForegroundColor Green
-  Write-Host "Last Run                   " -NoNewline 
+  Write-Host "Last Run                     " -NoNewline 
   Write-Host $job_items.LastRun
 
 # Bottleneck from Job log - v6 and later
   if ($vbo_version -gt 10) {
-  Write-Host "Bottleneck                 " -NoNewline 
+  Write-Host "Bottleneck                   " -NoNewline 
   $job_name = $j.Name
   $FullFileName = Get-ChildItem -Path $vbo_logpath\$job_name -Filter *.log | Where-Object {!$_.PSIsContainer} | Sort-Object {$_.LastWriteTime} -Descending | Select-Object -First 1
   ForEach ($File in $FullFileName)
@@ -113,8 +115,23 @@ ForEach ($j in $jobs) {
         $bottleneck = Select-String -Path $FilePath"\"$FileName -Pattern "Bottleneck:" | Select Line 
         Write-Host $bottleneck.Line
         }
+        # End bottleneck detection
+
+#Onedrive Scan for virus messages - V6+ only
+
+  If($job_items.SelectedItems.User.Count -ge 1 -And $job_items.SelectedItems.OneDrive -eq "True") {
+  Write-Host "One Drive Job Scan           " -NoNewline
+  $FullFileName = Get-ChildItem -Path $vbo_logpath\$job_name -Filter *.log | Where-Object {!$_.PSIsContainer} | Sort-Object {$_.LastWriteTime} -Descending | Select-Object -First 1
+  ForEach ($File in $FullFileName)
+        {
+        $FilePath = $File.DirectoryName
+        $FileName = $File.Name
+        $avmsg    = Select-String -Path $FilePath"\"$FileName -Pattern "virus reported" | Select Line 
+        Write-Host $avmsg[1] -ForegroundColor Yellow
         }
-# End bottleneck detection
+  }
+  }
+
 
   If($job_items.LastStatus -ne "Success") {$nok++}
   Write-Host " "
@@ -124,59 +141,75 @@ ForEach ($j in $jobs) {
 # Calculate the total objects (not used)
 #$total_objects=$orga+$exch+$exar+$od+$sites+$teams
 
-# Now some checking
+# Now some checks
 Write-Host "*******************************************************" -ForegroundColor Cyan
-Write-Host "*              VBO Environment Check                  *"
+Write-Host "*              VB365 Environment Check                *"
 Write-Host "*******************************************************" -ForegroundColor Cyan
 Write-Host "Backup Environment" -ForegroundColor Cyan
-Write-Host "Installed version          " -NoNewline
+Write-Host "Installed version            " -NoNewline
 Write-Host $vbo_version -ForegroundColor Green
 WriteLog   "Installed Version" $vbo_version
+Write-Host "Number of Backup Apps        " -NoNewline
+Write-Host $vbo_bkpapp.Count -ForegroundColor Green
+WriteLog   "Number of Backup Apps " $vbo_bkpapp.Count
+If ($vbo_bkpapp.Count -ge 1) {Write-Host "More than 1 Backup Application configured. Please remove!" -ForegroundColor Yellow} 
+Else {""}
 Write-Host ""
 if ($Webcheck -eq "Yes") {
-Write-Host "Latest available version   " -NoNewline
+Write-Host "Latest available version     " -NoNewline
 Write-Host $vbo_latestdisplayversion -ForegroundColor Green
-Write-Host "Please see this KB Article " -NoNewline
+Write-Host "Please see this KB Article   " -NoNewline
 Write-Host $vbo_latestversionkb -ForegroundColor Green
 Write-Host ""            }
-Write-Host "Number of Proxies          " -NoNewline
+Write-Host "               Proxy Server Setup                      "
+Write-Host "*******************************************************" -ForegroundColor Cyan
+Write-Host "Number of Proxies            " -NoNewline
 Write-Host $vbo_proxy.Count -ForegroundColor Green
 WriteLog   "Number of Proxies" $vbo_proxy.Count
-
 ForEach ($proxy in $vbo_proxy) {
-Write-Host "Proxy Name                 " -NoNewline
+Write-Host "Proxy Name                   " -NoNewline
 Write-Host $proxy.Hostname
 $proxy_cpu = (Get-WmiObject -Class Win32_Processor -ComputerName $proxy.Hostname)
-Write-Host "Number of CPUs             " -NoNewline
+Write-Host "Number of CPUs               " -NoNewline
 Write-Host $proxy_cpu.Count -ForegroundColor Green -NoNewline
 If ($proxy_cpu.Count -lt 4) {"More CPUs may be needed"} 
 Else {""}
 $proxy_mem = (Get-WMIObject -class win32_ComputerSystem -ComputerName $proxy.Hostname | % {$_.TotalPhysicalMemory})
 $proxy_memory = [math]::Round($proxy_mem/1024/1024/1024) 
-Write-Host "Amount of RAM (GB)         " -NoNewline
+Write-Host "Amount of RAM (GB)           " -NoNewline
 Write-Host $proxy_memory -ForegroundColor Green -NoNewline
+If ($proxy_memory -lt 16) {"    More RAM may be needed"} 
+Else {""}
+Write-Host "Number of configured threads " -NoNewline
+Write-Host $proxy.ThreadsNumber -ForegroundColor Green -NoNewline
 If ($proxy_memory -lt 16) {"    More RAM may be needed"} 
 Else {""}
 Write-Host
    }
+
 # Number of Repositories - No more checks
-Write-Host "Number of Repositories     " -NoNewline
+Write-Host "                Repository Server Setup                "
+Write-Host "*******************************************************" -ForegroundColor Cyan
+Write-Host "Number of Repositories       " -NoNewline
 Write-Host $vbo_repository.Count -ForegroundColor Green
 WriteLog "Number of Repositories" $vbo_repository.Count
 Write-Host
 ForEach ($repo in $vbo_repository) {
 If (!$repo.ObjectStorageRepository){
 Write-Host "Local Repository Usage"
-Write-Host "Repository Name:           " -NoNewline
+Write-Host "Repository Name:             " -NoNewline
 Write-Host $repo.Name
 WriteLog "Repository Name" $repo.name
 $vbo_repofree = [math]::Round(($repo.FreeSpace*100/$repo.Capacity)) 
-Write-Host "% Free Capacity:           " -NoNewline
+Write-Host "% Free Capacity:             " -NoNewline
 Write-Host $vbo_repofree -ForegroundColor Green
 WriteLog "% Free Capacity" $vbo_repofree
 }
 }
 Write-Host
+
+Write-Host "                   General Checks                      "
+Write-Host "*******************************************************" -ForegroundColor Cyan
 
 # Check logs if throttling during backup occured - All logs from current month get checked
 Write-Host "Did any throttling during backup occur in" $month $year "?         " -NoNewline
@@ -215,16 +248,16 @@ WriteLog "Low memory conditions on"$server.Hostname $mem_events.Count
 Write-Host
 
 # Print if a backup job is not in "Success" state
-Write-Host "Jobs not successfully run  " -NoNewline
+Write-Host "Jobs not successfully run    " -NoNewline
 Write-Host $nok -ForegroundColor Yellow
 If ($jobs.Count -le 1 -and $orga -gt 1) {"                           Only one Backup Job configured"} 
 Else {""}
-WriteLog "Jobs not successfully run" $jobs.Count
+WriteLog "Jobs not successfully run        " $jobs.Count
 
 # Licensed user & expiration date
-Write-Host "Licensed user              " -NoNewline
+Write-Host "Licensed users               " -NoNewline
 Write-Host $vbo_license.Count -ForegroundColor Green
-Write-Host "License expiration         " -NoNewline
+Write-Host "License expiration           " -NoNewline
 Write-Host $vbo_exp.SupportExpirationDate -ForegroundColor Green
 WriteLog "License expiration" $vbo_exp.SupportExpirationDate
 Write-Host
@@ -237,8 +270,10 @@ WriteLog "Possible Repository retention change activities"
 Write-Host
 
 # Check for Restore Sessions outside of business hours (7 to 17)
+Write-Host "Restore activites outside of business hours                  " -NoNewline
+If ($vbo_restore.Count -eq 0) {"No"}
 ForEach ($outside in $vbo_restore) {
-Write-Host "Restore started outside of business hours" -ForegroundColor Yellow
+Write-Host "Restore activity detected " -ForegroundColor Yellow
 Write-Host ""
 Write-Host "Start Time         " -NoNewline
 $outside.StartTime
