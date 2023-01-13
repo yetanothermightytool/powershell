6905a8929e882e20e0d43f3b9ba6dd6607d8c4cb
YAMT <94518822+yetanothermightytool@users.noreply.github.com>
2023-01-13 12:48:08 +0100

    Update m365-register-new-app.ps1

--- m365/unattended-script-example/m365-register-new-app.ps1
+++ m365/unattended-script-example/m365-register-new-app.ps1
@@ -1,24 +1,24 @@
 # Code example for registering an application and assigning the necessary permissions
-			# Application Name
-			$appName               = "exo_reporter"
+# Application Name
+$appName               = "exo_reporter"
 
-			#  Get the Microsoft 365 EXO API details
-			$api                   = (Get-AzureADServicePrincipal -Filter "AppID eq '00000002-0000-0ff1-ce00-000000000000'")
+#  Get the Microsoft 365 EXO API details
+$api                   = (Get-AzureADServicePrincipal -Filter "AppID eq '00000002-0000-0ff1-ce00-000000000000'")
 
-			# Get the permission ID
-			$permission            = $api.AppRoles | Where-Object { $_.Value -eq 'Exchange.ManageAsApp' }
+# Get the permission ID
+$permission            = $api.AppRoles | Where-Object { $_.Value -eq 'Exchange.ManageAsApp' }
 
-			# Build the permission object
-			$apiPermission         = [Microsoft.Open.AzureAD.Model.RequiredResourceAccess]@{
-									  ResourceAppId  = $api.AppId ;
-									  ResourceAccess = [Microsoft.Open.AzureAD.Model.ResourceAccess]@{
-									  Id   = $permission.Id ;
-									  Type = "Role"
-									   }
-									  }
+# Build the permission object
+$apiPermission = [Microsoft.Open.AzureAD.Model.RequiredResourceAccess]@{
+    ResourceAppId  = $api.AppId ;
+    ResourceAccess = [Microsoft.Open.AzureAD.Model.ResourceAccess]@{
+        Id   = $permission.Id ;
+        Type = "Role"
+    }
+}
 
-			# Register the newly created  Azure AD App with API Permissions
-			$myApp                 = New-AzureADApplication -DisplayName $appName -ReplyUrls 'http://localhost' -RequiredResourceAccess $apiPermission
+# Register the newly created  Azure AD App with API Permissions
+$myApp                 = New-AzureADApplication -DisplayName $appName -ReplyUrls 'http://localhost' -RequiredResourceAccess $apiPermission
 
-			# Enable the Service Principal
-			$mySP                  = New-AzureADServicePrincipal -AppID $myApp.AppID
+# Enable the Service Principal
+$mySP                  = New-AzureADServicePrincipal -AppID $myApp.AppID
