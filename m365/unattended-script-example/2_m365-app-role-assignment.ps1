c9600b5eb8299520d89bfed7a3ffa70f47745db7
YAMT <94518822+yetanothermightytool@users.noreply.github.com>
2023-01-15 14:25:58 +0100

    Update 2_m365-app-role-assignment.ps1

--- m365/unattended-script-example/2_m365-app-role-assignment.ps1
+++ m365/unattended-script-example/2_m365-app-role-assignment.ps1
@@ -1,8 +1,8 @@
 # Role to assign to the app
-$directoryRole = 'Exchange Administrator'
+$directoryRole = "Exchange Administrator"
 
-# Find the ObjectID of the 'Exchange Administrator' role
-$roleId = (Get-AzureADDirectoryRole | Where-Object {$_.displayname -eq $directoryRole}).ObjectID
+# Find the ObjectID of the "Exchange Administrator" role
+$roleId        = (Get-AzureADDirectoryRole | Where-Object {$_.displayname -eq $directoryRole}).ObjectID
 
 # Add the service principal to the directory role
 Add-AzureADDirectoryRoleMember -ObjectId $roleId -RefObjectId $mySP.ObjectID -Verbose
