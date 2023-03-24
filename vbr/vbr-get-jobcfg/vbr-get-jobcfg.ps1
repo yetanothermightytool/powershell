6ef349f6c58c230ee3562e4906f910bca21a5fb3
YAMT <94518822+yetanothermightytool@users.noreply.github.com>
2023-03-24 07:21:23 +0100

    Update vbr-get-jobcfg.ps1

--- vbr/vbr-get-jobcfg/vbr-get-jobcfg.ps1
+++ vbr/vbr-get-jobcfg/vbr-get-jobcfg.ps1
@@ -9,12 +9,13 @@
     Requires   : PowerShell, Veeam Backup & Replication v12
 
 .VERSION
-    1.0
+    1.1
 #>
 param(
 [Parameter(Mandatory = $false)]
     [Switch] $Retention,
-    [Switch] $Storage)
+    [Switch] $Storage,
+    [Switch] $NAS)
 
 # Set Variables
 $finalResult   = @()
@@ -23,11 +24,15 @@ $finalResult   = @()
 Connect-VBRServer -Server localhost
 
 # $vbrJobs = Get-VBRBackup
-$vbrJobs = Get-VBRJob -WarningAction SilentlyContinue | Where-Object { $_.JobType -eq 'Backup'}
+$vbrJobs       = Get-VBRJob -WarningAction SilentlyContinue | Where-Object { $_.JobType -eq 'Backup'}
+$vbrNASJobs    = Get-VBRNASBackupJob -WarningAction SilentlyContinue 
 
 # Output Storage Related Settings
 if($Storage){
 foreach ($jobs in $vbrJobs) {
+    
+    $opt               = $jobs.Options.BackupStorageOptions.StgBlockSize
+            
     $finalResult       += New-Object psobject -Property @{
     JobName            = $jobs.Name
     JobType            = $jobs.Jobtype
@@ -38,7 +43,7 @@ foreach ($jobs in $vbrJobs) {
     ActiveFull         = $jobs.Options.BackupStorageOptions.EnableFullBackup
     FullBackupDay      = $jobs.Options.BackupTargetOptions.FullBackupDays 
     CompressionLevel   = $jobs.Options.BackupStorageOptions.CompressionLevel
-    StorageOpt         = $opt = $jobs.Options.BackupStorageOptions.StgBlockSize
+    StorageOpt         = $opt -replace '^.*(?=.{4}$)'
     JobEncryption      = $jobs.Options.BackupStorageOptions.StorageEncryptionEnabled
       }   
     }
@@ -52,7 +57,7 @@ $finalResult | Format-Table -Wrap -AutoSize -Property @{Name='Job Name';Expressi
                                                       @{Name='Active Full';Expression={$_.ActiveFull};align='left'},
                                                       @{Name='Active Full on';Expression={$_.FullBackupDay};align='left'},
                                                       @{Name='Compression Level';Expression={$_.CompressionLevel};align='center'},
-                                                      @{Name='Storage Optimization';Expression={$_.StorageOpt};align='left'},
+                                                      @{Name='Storage Optimization';Expression={$_.StorageOpt};align='center'},
                                                       @{Name='Backup Encryption';Expression={$_.JobEncryption};align='left'}
                                                       
 
@@ -86,6 +91,32 @@ $finalResult | Format-Table -Wrap -AutoSize -Property @{Name='Job Name';Expressi
                                                       @{Name='GFS Monthly';Expression={$_.MonthlyRetention};align='center'},
                                                       @{Name='GFS Yearly Enabled';Expression={$_.YearlyGFSEnabled};align='left'},
                                                       @{Name='GFS Yearly';Expression={$_.YearlyRetention};align='center'}
+}
+
+# Output NAS Backup Job Parameters
+
+if($NAS){
+foreach ($jobs in $vbrNASJobs) {
+    $finalResult       += New-Object psobject -Property @{
+    JobName            = $jobs.Name
+    Path               = $jobs.BackupObject.Path
+    BackupIOControl    = $jobs.BackupObject.Server.BackupIOControlLevel
+    Repository         = $jobs.ShortTermBackupRepository.Name
+    RepositoryType     = $jobs.ShortTermBackupRepository.Type
+    RetentionType      = $jobs.ShortTermRetentionType
+    RetentionPeriod    = $jobs.ShortTermRetentionPeriod
+    JobEncryption      = $jobs.StorageOptions.EncryptionEnabled   
+      }   
+    }
+# Print Table
+$finalResult | Format-Table -Wrap -AutoSize -Property @{Name='Job Name';Expression={$_.JobName}},
+                                                      @{Name='Protected Path';Expression={$_.Path};align='left'},
+                                                      @{Name='BackupIOControl';Expression={$_.BackupIOControl};align='left'},
+                                                      @{Name='Repository';Expression={$_.Repository};align='left'},
+                                                      @{Name='Repository Type';Expression={$_.RepositoryType};align='left'},
+                                                      @{Name='Retention Type';Expression={$_.RetentionType};align='left'},
+                                                      @{Name='Retention';Expression={$_.RetentionPeriod};align='center'},
+                                                      @{Name='Backup Encryption';Expression={$_.JobEncryption};align='left'}
 }
 
 # Disconnect VBR Server Session
