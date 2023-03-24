93ab412a1b4173b851edce07e17ca1c2a3cc3c7d
YAMT <94518822+yetanothermightytool@users.noreply.github.com>
2023-03-24 16:43:29 +0100

    Create vbr-job-scanner.ps1

---
+++ vbr/vbr-job-scanner/vbr-job-scanner.ps1
@@ -0,0 +1,33 @@
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$JobName,
+    [Parameter(Mandatory=$true)]
+    [string]$Depth
+  )
+
+# Variables
+$finalResult   = @()
+$bkpJob          = Get-VBRJob -Name $JobName -WarningAction SilentlyContinue
+$bkpSession      = Get-VBRBackupSession| Where {$_.jobId -eq $bkpJob.Id.Guid} | Sort EndTimeUTC -Descending
+
+# Put the information together
+for ($i = 0; $i -le $bkpSession.count; $i++) {
+    foreach ($sessDetails in $bkpSession) {
+    $finalResult       += New-Object psobject -Property @{
+    TransferedSize     = $sessDetails[0].sessioninfo.Progress.TransferedSize[$i]
+               }   
+             }
+           }
+
+# Get the last x values (Depth) from the array
+$lastValues = $finalResult.TransferedSize[0..$Depth]
+
+# Calculate the average of the last x backups
+$average = ($lastValues | Measure-Object -Average).Average
+
+# Check if any of the last x backups are more than 10% larger than the average
+if (($lastValues | Where-Object { $_ -gt $average * 1.1 }).Count -gt 0) {
+    Write-Host "Unexpected growth detected in last $Dept Backup(s) values!"
+} else {
+    Write-Host "No unexpected growth detected in last $Depth Backup(s)."
+}                                                                                            
