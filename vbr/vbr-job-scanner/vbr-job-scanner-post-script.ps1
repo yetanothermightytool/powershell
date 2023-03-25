03e7591d5fc219db569a7048309cf859be865d13
YAMT <94518822+yetanothermightytool@users.noreply.github.com>
2023-03-25 09:32:04 +0100

    Update vbr-job-scanner-post-script.ps1

--- vbr/vbr-job-scanner/vbr-job-scanner-post-script.ps1
+++ vbr/vbr-job-scanner/vbr-job-scanner-post-script.ps1
@@ -1,9 +1,3 @@
-# Variables
-$finalResult     = @()
-#####CHANGE JOB NAME HERE#####
-$bkpJob          = Get-VBRJob -Name <jobname> -WarningAction SilentlyContinue
-$bkpSession      = Get-VBRBackupSession| Where {$_.jobId -eq $bkpJob.Id.Guid} | Sort EndTimeUTC -Descending
-
 # Funciton to get Process ID - Credits to Tom Sightler
 function Get-VbrJobSessionFromPID {
 $parentpid = (Get-WmiObject Win32_Process -Filter "processid='$pid'").parentprocessid.ToString()
@@ -13,8 +7,23 @@ $session   = Get-VBRBackupSession | ?{($_.OrigJobName -eq $job.Name) -and ($pare
     return $session
 }
 
+# Function to get JobName from running process
+function Get-VbrJobNameFromPID {
+    $parentPid = (Get-WmiObject Win32_Process -Filter "processid='$pid'").parentprocessid.ToString()
+    $parentCmd = (Get-WmiObject Win32_Process -Filter "processid='$parentPid'").CommandLine
+    $cmdArgs = $parentCmd.Replace('" "','","').Replace('"','').Split(',')
+    $jobName = (Get-VBRJob | ? {$cmdArgs[4] -eq $_.Id.ToString()}).Name
+    return $jobName
+}
+
+# Variables
+$finalResult     = @()
+$bkpJobName      = Get-VbrJobNameFromPID
+$bkpJob          = Get-VBRJob -Name $bkpJobName -WarningAction SilentlyContinue
+$bkpSession      = Get-VBRBackupSession| Where {$_.jobId -eq $bkpJob.Id.Guid} | Sort EndTimeUTC -Descending
+
 # Get Backup Session
-$BackupSession = Get-VbrJobSessionFromPID
+$BackupSession   = Get-VbrJobSessionFromPID
 
 # Put the information together
 for ($i = 0; $i -le $bkpSession.count; $i++) {
