acbe4e9d860abc0db734f264a4e24940da304593
YAMT <94518822+yetanothermightytool@users.noreply.github.com>
2023-08-25 09:38:03 +0200

    v1.2

--- vbo/vb365-exo-recovery/vb365-exo-recovery.ps1
+++ vbo/vb365-exo-recovery/vb365-exo-recovery.ps1
@@ -1,28 +1,37 @@
 <# 
 .NAME
-    Veeam Backup for Microsoft 365 Exchange Online Mailbox Recovery
+    Veeam Backup for Microsoft 365 Exchange Online Mailbox ResTORer
 .DESCRIPTION
-    A Powershell script to recover certain mailboxes from the last VB365 Exchange Online Restore point to another Microsoft organization.
-    The mailboxes to be restored must be provided in a CSV file. The file format is described in the readme on GitHub.
+    A Powershell script to recover certain mailboxes from the last VB365 Exchange Online Restore point to another Microsoft organization or
+    to a local Exchange server. The mailboxes to be restored must be provided in a CSV file and configured at the destination.
 .NOTES  
-    File Name  : vb365-exo-recovery.ps1
-    Author     : Stephan Herzig, Veeam Software (stephan.herzig@veeam.com)
-    Requires   : PowerShell, VB365 Exchange Online Backup, Configured Azure application in the target tenant, CSV file
+    File Name  : vb365-exo-resTORer.ps1
+    Author     : Stephan "Steve" Herzig, Veeam Software (stephan.herzig@veeam.com)
+    Requires   : PowerShell, VB365 Exchange Online Backup, Configured Azure application in the target tenant or a local Exchange server, CSV file
 .VERSION
-    1.1
+    1.2
 #>
-param(
-        [Parameter(Mandatory = $true)]
-        [String] $SrcVB365Org,
-        [String] $DstAppId,
-        [String] $DstAppCertFile,
-        [String] $RestoreList
-       )
+param (
+    [Parameter(Mandatory = $true)]
+    [String] $SrcVB365Org,
+    [Parameter(Mandatory = $true)]
+    [String] $RestoreList,
+    [Parameter(ParameterSetName='Local', Mandatory=$true)]
+    [Switch]$RestoreLocal,
+    [Parameter(ParameterSetName='Local', Mandatory=$true)]
+    [String]$LocalExchangeSrv,
+    [Parameter(ParameterSetName='M365', Mandatory=$true)]
+    [Switch]$RestoreM365,
+    [Parameter(ParameterSetName='M365', Mandatory=$true)]
+    [String] $DstAppId,
+    [Parameter(ParameterSetName='M365', Mandatory=$true)]
+    [String] $DstAppCertFile
+)
 Clear-Host
+
 # Prepare the environment
 $vb365org       = Get-VBOOrganization -Name $SrcVB365Org
 $csv            = Import-Csv $RestoreList -Delimiter ","
-$securePwd      = Read-Host -Prompt "Enter Certificate Password" -AsSecureString
 
 # Start Exchange Restore Session using the latest backup state
 Start-VBOExchangeItemRestoreSession -Organization $vb365org -LatestState | Out-Null
@@ -32,18 +41,45 @@ $session        = Get-VBOExchangeItemRestoreSession
 $counter        = $session.count-1 
 $database       = Get-VEXDatabase -Session $session[$counter] 
 
-# Now loop through the csv document
-foreach ($entry in $csv) {
+# Restore to local Exchange Server
+    if($RestoreLocal){
+    $creds          = Get-Credential -Message "Please enter your Exchange Admin (Local Server) credentials)" 
+    $exchangeCAS    = $LocalExchangeSrv
+    
+    # Now loop through the csv document
+    foreach ($entry in $csv) {
 
-# Get the Mailbox
-$exmailbox     = Get-VEXMailbox -Database $database -Name $entry.SourceMbx
+    # Get the Mailbox
+    $exmailbox     = Get-VEXMailbox -Database $database -Name $entry.SourceMbx
 
-# resTORer
-$resTORer       = Restore-VEXItem -Mailbox $exmailbox -ApplicationId $DstAppId -ApplicationCertificatePath $DstAppCertFile -ApplicationCertificatePassword $securePwd -OrganizationName $entry.DestOrg -Region Worldwide -TargetMailbox $entry.DestMbxName -RestoreDeletedItem
-$processedMbx   = $processedMbx+1
-$createdCount   = $createdCount+$resTORer.CreatedCount
-$skippedCount   = $skippedCount+$resTORer.SkippedCount
-$failedCount    = $failedCount+$resTORer.FailedCount
+    # resTORer
+    $resTORer       = Restore-VEXItem -Mailbox $exmailbox -Server $exchangeCAS -Credential $creds -TargetMailbox $entry.DestMbxName -RestoreDeletedItem 
+    $processedMbx   = $processedMbx+1
+    $createdCount   = $createdCount+$resTORer.CreatedCount
+    $skippedCount   = $skippedCount+$resTORer.SkippedCount
+    $failedCount    = $failedCount+$resTORer.FailedCount
+    }
+
+}
+
+# Restore to M365 Organization
+    if($RestoreM365){
+    $securePwd     = Read-Host -Prompt "Enter Certificate Password" -AsSecureString
+
+    # Now loop through the csv document
+    foreach ($entry in $csv) {
+
+    # Get the Mailbox
+    $exmailbox     = Get-VEXMailbox -Database $database -Name $entry.SourceMbx
+
+    # resTORer
+    $resTORer       = Restore-VEXItem -Mailbox $exmailbox -ApplicationId $DstAppId -ApplicationCertificatePath $DstAppCertFile -ApplicationCertificatePassword $securePwd -OrganizationName $entry.DestOrg -Region Worldwide -TargetMailbox $entry.DestMbxName -RestoreDeletedItem
+    $processedMbx   = $processedMbx+1
+    $createdCount   = $createdCount+$resTORer.CreatedCount
+    $skippedCount   = $skippedCount+$resTORer.SkippedCount
+    $failedCount    = $failedCount+$resTORer.FailedCount
+    }
+
 }
 
 # Build and show output
