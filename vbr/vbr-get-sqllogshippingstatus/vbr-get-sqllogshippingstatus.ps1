95d1aa4f2647688981dabffe5783ad02b2af5ca4
YAMT <94518822+yetanothermightytool@users.noreply.github.com>
2023-10-25 10:19:36 +0200

    adding RPO parameter

--- vbr/vbr-get-sqllogshippingstatus/vbr-get-sqllogshippingstatus.ps1
+++ vbr/vbr-get-sqllogshippingstatus/vbr-get-sqllogshippingstatus.ps1
@@ -1,7 +1,10 @@
 Param(
     [Parameter(Mandatory=$true)]
-    [string]$VBRServer
-    )
+    [string]$VBRServer,
+    [Parameter(Mandatory=$true)]
+    [int]$RPO
+)
+
 Clear-Host
 # Variables
 $sqlJobNames = @()
@@ -21,19 +24,34 @@ foreach ($vbrJob in $vbrJobs) {
     }
 }
 
-foreach ($sqlJobName in $sqlJobNames){
+foreach ($sqlJobName in $sqlJobNames) {
 
-$job                 = Get-VBRJob -name $sqlJobName
-$sqlJob              = $Job.FindChildSqlLogBackupJob()
-$session             = $sqlJob.FindLastSession()
-$taskSession         = Get-VBRTaskSession -Session $session
-$logBackupLogs       = $taskSession.Logger.GetLog().UpdatedRecords
-$lastSessionStartLog = $logBackupLogs | Where-Object { $_.Title.Contains("New transaction log backup interval started") } | Select -Last 1
-$lastSessionEndLog   = $logBackupLogs | Where-Object { $_.Title.Contains("Transaction log backup completed") } | Select -Last 1
+    $job                 = Get-VBRJob -name $sqlJobName
+    $sqlJob              = $Job.FindChildSqlLogBackupJob()
+    $session             = $sqlJob.FindLastSession()
+    $taskSession         = Get-VBRTaskSession -Session $session
+    $logBackupLogs       = $taskSession.Logger.GetLog().UpdatedRecords
+    $lastSessionStartLog = $logBackupLogs | ? { $_.Title.Contains("New transaction log backup interval started") } | Select -Last 1
+    $lastSessionEndLog   = $logBackupLogs | ? { $_.Title.Contains("Transaction log backup completed") } | Select -Last 1
 
-Write-Output $sqlJobName
-Write-Output "Start Time Last Log Session:" $lastSessionStartLog.StartTime
-Write-Output "Stop Time Last Log Session:" $lastSessionEndLog.StartTime
+    # Calculate the RPO time
+    $currentDateTime     = Get-Date
+    $rpoTime             = $lastSessionEndLog.StartTime.AddMinutes($RPO)
+
+    Write-Output $sqlJobName
+    Write-Output "Start Time Last Log Session: " $lastSessionStartLog.StartTime
+    Write-Output "Stop Time Last Log Session: " $lastSessionEndLog.StartTime
+    Write-Host
+
+    # Calculate the difference
+    $rpoDifference       = [Math]::Round(($currentDateTime - $rpoTime).TotalMinutes, 2)
+
+    # Check if RPO is greater than or equal to 0
+   if ($rpoDifference -ge 0) {
+    Write-Host "RPO exceeded by $rpoDifference minutes." -ForegroundColor Yellow
+} else {
+    Write-Host "RPO is within the specified time." -ForegroundColor Green
+}
 }
 
 Disconnect-VBRServer
