c9ffb454032e2c8aaa763957a8aa1be8127f4876
YAMT <94518822+yetanothermightytool@users.noreply.github.com>
2023-10-27 08:19:57 +0200

    v0.4
    Table output

--- vbr/vbr-get-sqllogshippingstatus/vbr-get-sqllogshippingstatus.ps1
+++ vbr/vbr-get-sqllogshippingstatus/vbr-get-sqllogshippingstatus.ps1
@@ -7,7 +7,7 @@ Param(
 
 Clear-Host
 # Variables
-$sqlJobNames = @()
+$sqlJobData = @()
 
 # Connect to VBR server
 Connect-VBRServer -Server $VBRServer
@@ -20,38 +20,38 @@ foreach ($vbrJob in $vbrJobs) {
     $sqlJobObjects = Get-VBRJobObject -Job $vbrJob | Where-Object { $_.VssOptions.SqlBackupOptions.BackupLogsEnabled -eq $true }
       
     if ($sqlJobObjects.Count -gt 0) {
-        $sqlJobNames += $vbrJob.Name
+        $sqlJobName = $vbrJob.Name
+        $sqlJob              = $vbrJob.FindChildSqlLogBackupJob()
+        $session             = $sqlJob.FindLastSession()
+        $taskSession         = Get-VBRTaskSession -Session $session
+        $logBackupLogs       = $taskSession.Logger.GetLog().UpdatedRecords
+        $lastSessionStartLog = $logBackupLogs | ? { $_.Title.Contains("New transaction log backup interval started") } | Select -Last 1
+        $lastSessionEndLog   = $logBackupLogs | ? { $_.Title.Contains("Transaction log backup completed") } | Select -Last 1
+
+        # Calculate the RPO time
+        $currentDateTime     = Get-Date
+        $rpoTime             = $lastSessionEndLog.StartTime.AddMinutes($RPO)
+
+        # Calculate the difference
+        $rpoDifference       = [Math]::Round(($currentDateTime - $rpoTime).TotalMinutes, 2)
+
+        # Determine if RPO is good (yes/no) and choose a color
+        if ($rpoDifference -ge 0) {
+            $rpoStatus = "no"
+        } else {
+            $rpoStatus = "yes"
+        }
+
+        $sqlJobData += [PSCustomObject]@{
+            "Job Name"   = $sqlJobName
+            "Start Time" = $lastSessionStartLog.StartTime
+            "Stop Time"  = $lastSessionEndLog.StartTime
+            "RPO good"   = $rpoStatus
+            
+        }
     }
 }
 
-foreach ($sqlJobName in $sqlJobNames) {
-
-    $job                 = Get-VBRJob -name $sqlJobName
-    $sqlJob              = $Job.FindChildSqlLogBackupJob()
-    $session             = $sqlJob.FindLastSession()
-    $taskSession         = Get-VBRTaskSession -Session $session
-    $logBackupLogs       = $taskSession.Logger.GetLog().UpdatedRecords
-    $lastSessionStartLog = $logBackupLogs | ? { $_.Title.Contains("New transaction log backup interval started") } | Select -Last 1
-    $lastSessionEndLog   = $logBackupLogs | ? { $_.Title.Contains("Transaction log backup completed") } | Select -Last 1
-
-    # Calculate the RPO time
-    $currentDateTime     = Get-Date
-    $rpoTime             = $lastSessionEndLog.StartTime.AddMinutes($RPO)
-
-    Write-Output $sqlJobName
-    Write-Output "Start Time Last Log Session: " $lastSessionStartLog.StartTime
-    Write-Output "Stop Time Last Log Session: " $lastSessionEndLog.StartTime
-    Write-Host
-
-    # Calculate the difference
-    $rpoDifference       = [Math]::Round(($currentDateTime - $rpoTime).TotalMinutes, 2)
-
-    # Check if RPO is greater than or equal to 0
-   if ($rpoDifference -ge 0) {
-    Write-Host "RPO exceeded by $rpoDifference minutes." -ForegroundColor Yellow
-} else {
-    Write-Host "RPO is within the specified time." -ForegroundColor Green
-}
-}
+$sqlJobData | Format-Table -AutoSize -Property "Job Name", "Start Time", "Stop Time", "RPO good"
 
 Disconnect-VBRServer
