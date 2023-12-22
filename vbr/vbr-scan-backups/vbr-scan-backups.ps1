288041bbe28b4f5213dd3fb9ad3005dfd6c40e55
YAMT <94518822+yetanothermightytool@users.noreply.github.com>
2023-12-22 09:07:49 +0100

    v1.1

--- vbr/vbr-scan-backups/vbr-scan-backups.ps1
+++ vbr/vbr-scan-backups/vbr-scan-backups.ps1
@@ -1,8 +1,24 @@
+<# 
+.NAME
+    Veeam Backup & Replication - Scan Backups
+.DESCRIPTION
+    This script retrieves information about a specified backup job and identifies backup objects for a target host. It prompts 
+    the user to select YARA rules for scanning, waits for input, and defaults to using all rules if none is provided within 30 seconds. 
+    The script then scans each backup object with the selected or all YARA rules.
+ .NOTES  
+    File Name  : vbr-scan-backups.ps1
+    Author     : Stephan "Steve" Herzig
+    Requires   : PowerShell, Veeam Backup & Replication v12.1
+.VERSION
+1.1
+#>
 Param(
     [Parameter(Mandatory=$true)]
     [string]$Jobname,
     [Parameter(Mandatory=$true)]
-    [string]$HostToScan
+    [string]$HostToScan,
+    [Switch]$YARAScan,
+    [Switch]$AVScan
      )
 Clear-Host
 Connect-VBRServer -Server localhost
@@ -12,14 +28,18 @@ $host.ui.RawUI.WindowTitle = "VBR Scan Backup"
 $backup                    = Get-VBRBackup | Where-Object { $_.jobname -eq $Jobname } 
 $bkpObjects                = Get-VBRBackupObject -Backup $backup | Where-Object {$_.IsLinux -ne "False" -and $_.Name -eq $HostToScan}
 $yaraRules                 = Get-VBRYARARule
-
 # No backup no scan
+
 if ($bkpObjects.Count -eq 0) {
 	Write-Host 'Unable to locate any restore points for scanning in backup job' $Jobname 'for host' $HostToScan -ForegroundColor Yellow
     Disconnect-VBRServer
 	Exit
 
-} else {
+} 
+
+# YaraScan 
+if ($YARAScan){
+
     $timeoutSeconds = 30
     $stopTime = [datetime]::Now.AddSeconds($timeoutSeconds)
 
@@ -60,13 +80,11 @@ if ($bkpObjects.Count -eq 0) {
         }
     } else {
         
-        Write-Host "No YARA rule selected. Using all YARA rules." -ForegroundColor Yellow
+        Write-Host "No YARA rule selected! Using all YARA rules." -ForegroundColor Yellow
         $selectedYaraRules = $yaraRules
        }
 
-    }
- 
- # Start scanning
+  # Start scanning
     foreach ($bkpObject in $bkpObjects) {
     Write-Host "Processing object: $bkpObject" -ForegroundColor White
 
@@ -77,6 +95,23 @@ if ($bkpObjects.Count -eq 0) {
         $result = Start-VBRScanBackup -Object $bkpObject -ScanMode FirstInInterval -EnableYARAScan -YARARule $yaraRule
     }
         Write-Host "Finished processing object: $object"
+  }
+Disconnect-VBRServer
+Exit 
+}
+
+if ($AVScan){
+
+  # Start scanning
+    foreach ($bkpObject in $bkpObjects) {
+    Write-Host "Processing object: $bkpObject" -ForegroundColor White
+              
+        $result = Start-VBRScanBackup -Object $bkpObject -ScanMode FirstInInterval -EnableAntivirusScan 
     }
- 
+        Write-Host "Finished processing object: $bkpObject"
+   
+Disconnect-VBRServer
+exit
+}
+
 Disconnect-VBRServer
